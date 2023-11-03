d1d4413a798fdedfd673e5fc0a9cd831ce2319c1
ThatOneGuy1211 <112908023+ThatOneGuy1211@users.noreply.github.com>
2023-11-02 17:47:06 -0700

    Add files via upload
    Updated sign in to include text boxes and a more sign in looking UI

--- frontend/Screens/SignIn.js
+++ frontend/Screens/SignIn.js
@@ -4,25 +4,67 @@ import {
     Text,
     Button,
     View,
+    TextInput,
+    StyleSheet,
+    TouchableOpacity,
 } from 'react-native';
 
 const SignIn = ({navigation})=>{
     return(
         <View>
-            <Button
-                title="Login"
-                onPress={() => navigation.navigate('UserHome', {
-                    userType: 1
-                })}
+            <Text style={styles.text}>Username/Email</Text>
+            <TextInput
+                style={styles.input}
+                
             />
-            <Button
-                title="Don't Have an Account?"
-                onPress={() => navigation.navigate('Sign Up', {
-                    msg: "This is a message"
-                })}
+            <Text style={styles.text}>Password</Text>
+            <TextInput
+                style={styles.input}
+                
             />
+            <View style={styles.bigButton}>
+                <Button
+                    title="Login"
+                    onPress={() => navigation.navigate('UserHome', {
+                        userType: 1
+                    })}
+                />
+            </View>
+            <View style={styles.linkButton}>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate('Sign Up', {
+                        msg: "This is a message"
+                    })}
+                >
+                    <Text style={styles.linkText}>Don't Have An Account?</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 export default SignIn;
+
+const styles = StyleSheet.create({
+    input: {
+        margin: 12,
+        borderWidth: 1,
+        height: 40,
+    },
+    text: {
+        left: 12,
+        top: 5,
+        bottom: 5,
+    },
+    bigButton: {
+        margin: 12,
+    },
+    linkButton: {
+        padding: 8,
+        backgroundColor: 'transparent',
+    },
+    linkText: {
+        color: 'blue',
+        textDecorationLine: 'underline',
+    },
+})
