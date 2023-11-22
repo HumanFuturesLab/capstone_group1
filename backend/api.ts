69f96b1964620fbee7e139403af6061af65c1587
Justin Kereszturi <jkereszt@asu.edu>
2023-11-22 16:42:43 -0700

    some of the interaction

--- backend/api.ts
+++ backend/api.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { Pool, Client } from "pg";
+import { UUID } from "bson";
 
 // express app
 const app = express();
@@ -9,6 +10,7 @@ const startApi = async () => {
   const client = new Client({
     user: "postgres",
     host: "localhost",
+    database: "gleo",
     password: "postgres",
     port: 5432,
   });
@@ -18,6 +20,43 @@ const startApi = async () => {
     } else {
       // connected now do stuff
       console.error("connected");
+
+      function isValidObjectId(id: string): boolean {
+        return UUID.isValid(id);
+      }
+
+      // Return data object of users
+      app.get("/users", async (req, res) => {
+        let x = await client.query("SELECT * FROM users;");
+        res.json({ data: x["rows"] });
+      });
+
+      //Return array of rewards
+      app.get("/rewards/:userID", async (req, res) => {
+        let param = req.params.userID;
+
+        if (!isValidObjectId(param)) {
+          res.status(400);
+          res.json({ data: "Invalid ID" });
+          return;
+        }
+
+        let UsersJOINOrders = await client.query(
+          "SELECT rewardID FROM Orders WHERE Orders.userID = '" + param + "';"
+        );
+
+        let OrdersJOINRewards = await client.query(
+          "SELECT Rewards.pointcost FROM Rewards JOIN Orders ON Rewards.rewardID = '" +
+            UsersJOINOrders["rows"][0]["rewardid"] +
+            "';"
+        );
+
+        res.json(OrdersJOINRewards["rows"]);
+      });
+
+      app.listen(3000, () => {
+        console.log("listening on 3000");
+      });
     }
   });
 };
