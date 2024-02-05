f0a203a6236a99fcf75982186a3fb35beaa3712f
Justin Kereszturi <justinkereszturi@gmail.com>
2024-02-05 10:36:35 -0700

    display the name of the user from the context

--- frontend/screens/Profile.tsx
+++ frontend/screens/Profile.tsx
@@ -1,8 +1,10 @@
 import {Button, SafeAreaView, StyleSheet, Text} from 'react-native';
-import React from 'react';
+import React, {useContext} from 'react';
 import {useAuth0} from 'react-native-auth0';
+import {Context} from '../context';
 
 const Profile = () => {
+  const value = useContext(Context);
   const {clearSession} = useAuth0();
   const onLogout = async () => {
     await clearSession({}, {});
@@ -12,6 +14,7 @@ const Profile = () => {
     <SafeAreaView>
       <Text>Profile</Text>
       <Button onPress={onLogout} title={'Log Out'} />
+      <Text>{value.name}</Text>
     </SafeAreaView>
   );
 };
