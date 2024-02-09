efcd6ed543bb11b6e7cb3fd35d5e0d97f9fc956f
topsu4 <acontr56@asu.edu>
2024-02-08 21:59:34 -0700

    added reward info to onpress

--- frontend/components/RewardDetails.tsx
+++ frontend/components/RewardDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Alert} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {Reward} from '../screens/Rewards';
 
@@ -24,7 +24,34 @@ const RewardDetails = ({reward}: RewardProps) => {
 
   return (
     <View style={styles.container}>
-      <Text>do stuff</Text>
+      <Text style={styles.title}>{reward.rewardname}</Text>
+      {reward.rewarddesc && (
+        <Text style={styles.description}>
+          Description: {reward.rewarddesc}
+        </Text>
+      )}
+      {reward.pointcost && (
+        <View style={styles.detailRow}>
+          <Icon name="star" size={20} />
+          <Text
+            style={styles.detailText}>{`${reward.pointcost} Points`}</Text>
+        </View>
+      )}
+      {reward.numavailable&& (
+        <View style={styles.detailRow}>
+          <Text style={styles.detailText}>{`${reward.numavailable} remaining`}</Text>
+        </View>
+      )}
+      <Text></Text>
+      <TouchableOpacity
+            onPress={() => {
+              Alert.alert('Do stuff');
+            }}
+            style={styles.button}
+            activeOpacity={0.8}>
+            <Text style={styles.buttonText}>Purchase</Text>
+          </TouchableOpacity>
+          <Text></Text>
     </View>
   );
 };
@@ -50,6 +77,26 @@ const styles = StyleSheet.create({
   description: {
     marginTop: 10,
     fontSize: 16,
+  },
+  button: {
+    backgroundColor: '#007bff', // Bootstrap primary color
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: '#007bff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 4},
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 3, // For Android shadow effect
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
